c473dd07b9da169db17702bff5aafdd43fe510af
huseyinasarkaya <asarkayas85@icloud.com>
2023-05-21 20:50:53 +0300

    ikinciGun2

--- src/G01_gitHub_ilkDers.java
+++ src/G01_gitHub_ilkDers.java
@@ -1,9 +1,10 @@
-public class gitHub_ilkDers {
+public class G01_gitHub_ilkDers {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
         System.out.println("=============");
         System.out.println("have a nice day");
         System.out.println("gitadd-commit-push");
+        System.out.println("tekrar merhaba github");
 
 
     }
---
+++ src/G02_gitHub_İlkDers.java
@@ -0,0 +1,6 @@
+public class G02_gitHub_İlkDers {
+    public static void main(String[] args) {
+
+
+    }
+}
