8498c55e7f8659354f83330264c0dd9f661f07ac
huseyinasarkaya <asarkayas85@icloud.com>
2023-06-06 21:18:49 +0300

    ikinciGun2

---
+++ src/G02_gitHub_ilkDers.java
@@ -0,0 +1,7 @@
+public class G02_gitHub_ilkDers {
+    public static void main(String[] args) {
+        System.out.println("Hello Again");
+        System.out.println("===================");
+
+    }
+}
--- src/G02_gitHub_İlkDers.java
+++
@@ -1,6 +0,0 @@
-public class G02_gitHub_İlkDers {
-    public static void main(String[] args) {
-
-
-    }
-}
